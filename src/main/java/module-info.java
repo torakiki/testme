859c2e094deb12ab9c8b0b7a91950c19eccb176a
torakiki <andrea.vacondio@gmail.com>
2021-02-10 12:40:15 +0100

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * @author Andrea Vacondio
+ *
+ */
+module black.pdf.model {
+    exports test.me.child;
+}
---
+++ src/main/java/test/me/child/OpenFileRequest.java
@@ -0,0 +1,5 @@
+package test.me.child;
+
+public class OpenFileRequest {
+	// NOOP
+}
